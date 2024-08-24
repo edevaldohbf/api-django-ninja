213331588ff1b145d6a8a3eace22ecf8d992a46e
Edevaldo <edevaldohbfilho@gmail.com>
2024-08-24 06:32:58 -0300

    feat(rural-producers): adding fields to model

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ from config.env import (DATABASE_HOST, DATABASE_NAME, DATABASE_PASSWORD,
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-sys.path.insert(0, os.path.join(BASE_DIR, "src"))
+sys.path.insert(0, os.path.join(BASE_DIR, "modules"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
